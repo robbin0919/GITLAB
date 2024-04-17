b7d6d3cb0d42245873a6535d7741c8c038f14a3b
robbin0919 <robbin0919@gmail.com>
2024-04-17 23:49:12 +0800

    EEE

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,12 +2,7 @@
 
 int main(void) {
     printf("Hello! World!\n");
-<<<<<<< HEAD
     printf("哈囉！AAA 語言！\n");
     printf("哈囉！C 語言！\n");
-=======
-    printf("哈囉！DD 語言！\n");
-
->>>>>>> f4cb79b67517d20312b2f55666bd9e1778b8a523
     return 0;
 }
