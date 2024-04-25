eee36f2e41348062751a96498eac756e49d66313
robbin lee <66932401+robbin0919@users.noreply.github.com>
2024-04-25 13:31:29 +0800

    Update HelloWorld.c

--- MergeCase/HelloWorld.c
+++ MergeCase/HelloWorld.c
@@ -3,5 +3,11 @@
 #include <stdio.h>
 int main()
 {
+    int num, count, sum = 0;
+    // for loop terminates when num is less than count
+    for(count = 1; count <= num; ++count)
+    {
+    sum += count;
     printf("Hello Robbin !!");
+    }
 }
