5801a5ac0907d48c8fe226af6fb4ac300919c4fd
robbin lee <66932401+robbin0919@users.noreply.github.com>
2024-05-08 21:52:54 +0800

    Update PR_test.c

--- PR/PR_test.c
+++ PR/PR_test.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
 int main() {
+   int i;
 
   // print Hello World to the screen
   printf("Hello World");
   printf("Hello World");
+
+  for (i = 1; i < 11; ++i)
+  {
+    printf("%d ", i);
+  }
   return 0;
-}
+  }
