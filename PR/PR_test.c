90b87d52d9a66581431fecc8fcfbd88de48d741a
WIN11 <robbin0919@gmail.com>
2024-05-08 21:57:24 +0800

    modified:   PR/PR_test.c

--- PR/PR_test.c
+++ PR/PR_test.c
@@ -2,14 +2,20 @@
 
 int main() {
    int i;
+   int number;
 
   // print Hello World to the screen
   printf("Hello World");
-  printf("Hello World");
-
+  printf("Enter an integer: ");
+  scanf("%d", &number);
+    // true if number is less than 
+    
+    if (number < 10) {
+        printf("You entered %d.\n", number);
+    }
   for (i = 1; i < 11; ++i)
-  {
-    printf("%d ", i);
-  }
+    {
+      printf("%d ", i);
+    }
   return 0;
   }
